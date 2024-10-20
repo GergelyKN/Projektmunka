4618f0cc5837f7149755135e70e3f865484e7f66
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-10-20 22:57:58 +0200

    Contact komponens létrehozása

---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+function Contact() {
+  return (
+    <>
+      <NavBar />
+      <h3>Kapcsolat menüpont</h3>
+      <Footer />
+    </>
+  );
+}
+export default Contact;
