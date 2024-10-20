db4e8acc8d0850f35853ca771c111ca6909b98ec
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-10-20 22:59:52 +0200

    Új útvonalak hozzáadása

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,6 +1,9 @@
 import MainPage from "../components/MainPage";
 import Drinks from "../components/Drinks";
 import ErrorPage from "../components/ErrorPage";
+import BoardGames from "../components/BoardGames";
+import Reservation from "../components/Reservation";
+import Contact from "../components/Contact";
 
 const routes = [
   {
@@ -11,6 +14,19 @@ const routes = [
   {
     path: "/italok",
     element: <Drinks />,
+  },
+  {
+    path: "/tarsasjatekok",
+    element: <BoardGames />,
+  },
+  {
+    path: "/foglalas",
+    element: <Reservation />,
+  },
+
+  {
+    path: "/kapcsolat",
+    element: <Contact />,
   },
 ];
 
