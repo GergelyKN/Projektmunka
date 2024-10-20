8554310d850a9b111b2a9460f42019fbd1528ecc
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-10-20 22:59:33 +0200

    Reservation komponens létrehozása

---
+++ frontend/src/components/Reservation.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+function Reservation() {
+  return (
+    <>
+      <NavBar />
+      <h3>Foglalás menüpont</h3>
+      <Footer />
+    </>
+  );
+}
+export default Reservation;
