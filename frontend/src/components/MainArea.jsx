56eb19f889d9ad16b18221c77f2f22f8f8f54843
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-10-20 22:21:27 +0200

    MainArea komponens létrehozása

---
+++ frontend/src/components/MainArea.jsx
@@ -0,0 +1,33 @@
+function MainArea() {
+  return (
+    <article>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+        suscipit beatae assumenda quibusdam neque in impedit asperiores illum
+        animi, est tempora ullam, autem magni veniam repudiandae repellat! Quam,
+        cumque? Minima. Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Quo nisi esse ducimus sequi voluptates voluptate recusandae vero
+        ex dignissimos soluta, illum quasi sapiente, fugit amet ullam tempora
+        totam saepe impedit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Laudantium eos nisi esse, dignissimos dolorem consequuntur a
+        temporibus expedita voluptate commodi culpa optio facere earum aliquam
+        sed veritatis itaque ipsa id. Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Aperiam tempora dolor sequi voluptate? At ratione id,
+        asperiores tempora dolorum voluptatum! Odit itaque officiis neque hic
+        eveniet tempora a mollitia similique? Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Blanditiis sequi enim cumque sunt, unde
+        dolorem ad consectetur iste voluptates architecto? Tenetur repudiandae
+        maiores nemo fugit neque earum repellat ab beatae? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Voluptates accusantium nulla
+        exercitationem nemo reiciendis id tempore aspernatur provident magni
+        asperiores fugiat fuga dolores, similique ratione quia hic, pariatur
+        laudantium sunt. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Pariatur, tempore architecto excepturi assumenda aliquid officia
+        sint. Sunt aperiam praesentium ratione doloribus! Repellendus
+        perferendis blanditiis pariatur fugiat quas sequi a iusto.
+      </p>
+    </article>
+  );
+}
+
+export default MainArea;
