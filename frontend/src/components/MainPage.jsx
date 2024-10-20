d28d35269fb568a20d01555e462414aaeb425b2b
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-10-20 22:21:36 +0200

    MainPage komponens létrehozása

---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import MainArea from "./MainArea";
+
+function MainPage() {
+  return (
+    <>
+      <NavBar />
+      <MainArea />
+      <Footer />
+    </>
+  );
+}
+
+export default MainPage;
