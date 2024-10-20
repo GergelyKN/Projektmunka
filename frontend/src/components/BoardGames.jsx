c25949fbc285bb150473a5e3dd11c2b00f946499
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-10-20 22:57:46 +0200

    BoardGames komponens létrehozása

---
+++ frontend/src/components/BoardGames.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+function BoardGames() {
+  return (
+    <>
+      <NavBar />
+      <h3>Társasjáték menüpont</h3>
+      <Footer />
+    </>
+  );
+}
+export default BoardGames;
