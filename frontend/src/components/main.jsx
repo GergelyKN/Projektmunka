bf8b41c8b9e3abf8d9738f8edb9232367526c08f
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-10-28 12:14:07 +0100

    Kezdetleges stílus hozzáadása

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "../routes/routes";
-
-//Kell egy jobb név
+import "../styles/rootStyle.css";
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
