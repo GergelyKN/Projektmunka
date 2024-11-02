b7320e9d532604326dbeada270e6192cc892ea58
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-02 02:15:28 +0100

    Routerek hozzáadása

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 const port = process.env.PORT;
+const messagesRouter = require("./routes/messagesRouter");
+const drinksRouter = require("./routes/drinksRouter");
+const boardGamesRouter = require("./routes/boardGamesRouter");
+const usersRouter = require("./routes/usersRouter");
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/message", (req, res) => {
-  res.json({ message: "Hello World!!" });
-});
+app.use("/api/messages", messagesRouter);
+app.use("/api/drinks", drinksRouter);
+app.use("/api/boardgames", boardGamesRouter);
+app.use("/api/users", usersRouter);
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port + "!");
