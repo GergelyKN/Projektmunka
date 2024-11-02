5a719d26d7717d357c2ab85b1050542a1c662bc4
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-02 02:22:48 +0100

    Működik a form

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,7 +1,61 @@
 import NavBar from "../Helper_Components/NavBar";
 import Footer from "../Helper_Components/Footer";
+import { useState } from "react";
 
 function Contact() {
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Text, setText] = useState("");
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const message = {
+      ContactFirstName: FirstName,
+      ContactLastName: LastName,
+      ContactEmail: Email,
+      ContactText: Text,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/api/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(message),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        alert(data.message);
+      } else {
+        const errorData = await response.json();
+        alert(errorData.error);
+      }
+    } catch (error) {
+      console.error("Hiba történt a kapcsolódáskor: ", error);
+    } finally {
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setText("");
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -18,73 +72,81 @@ function Contact() {
           </thead>
           <tr>
             <td>Hétfő</td>
-            <td>15:00 - 22:00</td>
+            <td>10:00 - 01:00</td>
           </tr>
           <tr>
             <td>Kedd</td>
-            <td>15:00 - 22:00</td>
+            <td>10:00 - 01:00</td>
           </tr>
           <tr>
             <td>Szerda</td>
-            <td>15:00 - 01:00</td>
+            <td>10:00 - 01:00</td>
           </tr>
           <tr>
             <td>Csütörtök</td>
-            <td>15:00 - 22:00</td>
+            <td>10:00 - 01:00</td>
           </tr>
           <tr>
             <td>Péntek</td>
-            <td>15:00 - 02:00</td>
+            <td>10:00 - 02:00</td>
           </tr>
           <tr>
             <td>Szombat</td>
-            <td>15:00 - 02:00</td>
+            <td>10:00 - 02:00</td>
           </tr>
           <tr>
             <td>Vasárnap</td>
-            <td>15:00 - 00:00</td>
+            <td>10:00 - 01:00</td>
           </tr>
         </table>
       </div>
       <div className="uzenet">
-        <form action="#">
+        <form action="/api/messages" method="POST" onSubmit={handleSubmit}>
           <fieldset>
             <h3>Írj nekünk!</h3>
-            <label htmlFor="cvnev">Vezetéknév </label>
+            <label htmlFor="ContactLastName">Vezetéknév </label>
             <input
               type="text"
-              id="cvnev"
-              name="cvnev"
+              id="ContactLastName"
+              name="ContactLastName"
               required
               placeholder="Varga"
+              value={LastName}
+              onChange={handleLastNameChange}
             />
-            <label htmlFor="cknev">Keresztnév </label>
+            <label htmlFor="ContactFirstName">Keresztnév </label>
             <input
               type="text"
-              id="cknev"
-              name="cknev"
+              id="ContactFirstName"
+              name="ContactFirstName"
               required
               placeholder="Károly"
+              value={FirstName}
+              onChange={handleFirstNameChange}
             />
-            <label htmlFor="cemail">Email cím </label>
+            <label htmlFor="ContactEmail">Email cím </label>
             <input
               type="email"
-              id="cemail"
-              name="cemail"
+              id="ContactEmail"
+              name="ContactEmail"
               required
               placeholder="valami@email.com"
+              value={Email}
+              onChange={handleEmailChange}
             />
-            <label htmlFor="ctext">Üzenet </label>
+            <label htmlFor="ContactText">Üzenet </label>
             <textarea
-              name="ctext"
-              id="ctext"
+              name="ContactText"
+              id="ContactText"
               required
               placeholder="Üzenj nekünk valamit!"
               maxLength={350}
               rows={15}
               cols={30}
+              value={Text}
+              onChange={handleTextChange}
             ></textarea>
-            <button id="csendform" type="submit">
+            <button id="ContactSendForm" type="submit">
               Küldés
             </button>
           </fieldset>
