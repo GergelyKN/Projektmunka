9fe932693893176ffa8c0c30468264e54bc07436
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-03 23:54:16 +0100

    Bejelentkezési funckió létrehozása

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,4 +1,7 @@
 const db = require("../model/usersQueries/usersQuery");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 async function postNewUser(req, res) {
   const { regFirstName, regLastName, regEmail, regPassword } = req.body;
@@ -7,7 +10,14 @@ async function postNewUser(req, res) {
   }
 
   try {
-    await db.postNewUser({ regFirstName, regLastName, regEmail, regPassword });
+    const hashedPassword = await bcrypt.hash(regPassword, 10);
+
+    await db.postNewUser({
+      regFirstName,
+      regLastName,
+      regEmail,
+      hashedPassword,
+    });
     return res.status(201).json({ message: "Felhasználó rögzítve" });
   } catch (error) {
     console.error("Sikertelen rögzítés: ", error);
@@ -17,4 +27,44 @@ async function postNewUser(req, res) {
   }
 }
 
-module.exports = { postNewUser };
+async function postLogin(req, res) {
+  const { loginEmail, loginPassword } = req.body;
+  if (!loginEmail || !loginPassword) {
+    return res.status(400).json({ error: "Hiányzó mezők" });
+  }
+  try {
+    const user = await db.getUserByEmail(loginEmail);
+
+    if (user.length === 0) {
+      return res.status(401).json({ error: "Felhasználó nem található" });
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      loginPassword,
+      user[0].hashedpassword
+    );
+    if (!isValidPassword) {
+      return res.status(404).json({ error: "Helytelen email vagy jelszó" });
+    }
+
+    const token = jwt.sign(
+      {
+        userID: user[0].userid,
+        isAdmin: user[0].isAdmin,
+      },
+      process.env.TOKENKEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return res
+      .status(201)
+      .json({ token, user, message: "Sikeres bejelentkezés" });
+  } catch (error) {
+    console.error("Sikertelen bejelentkezés: ", error);
+    return res.status(500).json({ error: "Hiba történt a bejelentkezéskor" });
+  }
+}
+
+module.exports = { postNewUser, postLogin };
