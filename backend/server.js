090ccdb46c2d0fe3538c2138cb0c63ee970dab4c
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-03 23:57:59 +0100

    --

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT;
+
+const port = process.env.PORT || 3000;
+
 const messagesRouter = require("./routes/messagesRouter");
 const drinksRouter = require("./routes/drinksRouter");
 const boardGamesRouter = require("./routes/boardGamesRouter");
