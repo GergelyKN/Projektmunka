8401e050ebdda7b059ce3dcffd6c662237135534
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-03 23:57:47 +0100

    login útvonalkezelő létrehozása

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -3,5 +3,6 @@ const usersController = require("../controllers/usersController");
 const usersRouter = Router();
 
 usersRouter.post("/registration", usersController.postNewUser);
+usersRouter.post("/login", usersController.postLogin);
 
 module.exports = usersRouter;
