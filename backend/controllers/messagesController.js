592b2f878b806d64bfd503a5d1a3abc5f5e4cd1a
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-04 13:46:20 +0100

    Email küldés

--- backend/controllers/messagesController.js
+++ backend/controllers/messagesController.js
@@ -1,4 +1,7 @@
+const { text } = require("express");
 const db = require("../model/messagesQueries/messagesQuery");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 async function postNewMessage(req, res) {
   const { ContactFirstName, ContactLastName, ContactEmail, ContactText } =
@@ -15,12 +18,42 @@ async function postNewMessage(req, res) {
       ContactText,
     });
 
+    await sendEmail(
+      ContactText,
+      ContactEmail,
+      ContactLastName,
+      ContactFirstName
+    );
+
     return res.status(201).json({ message: "Üzenet sikeresen rögzítve" });
   } catch (error) {
     console.error("Sikertelen rögzítés: ", error);
     return res
       .status(500)
       .json({ error: "Hiba történt az üzenet rögzítésekor" });
+  }
+}
+
+async function sendEmail(message, email, lastname, firstname) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAILPASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: `Új üzenet érkezett ${lastname} ${firstname}-tól/től`,
+    text: `${email} címről érkezett az alábbi üzenet:\n${message}`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sikeresen elküldve!");
+  } catch (err) {
+    console.error("Email küldési hiba: ", err);
   }
 }
 
