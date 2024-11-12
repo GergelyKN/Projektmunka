906ca7f69d4fe535bb53c753c2e69d734034ac7e
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-12 12:39:44 +0100

    adminRouter használata

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const messagesRouter = require("./routes/messagesRouter");
 const drinksRouter = require("./routes/drinksRouter");
 const boardGamesRouter = require("./routes/boardGamesRouter");
 const usersRouter = require("./routes/usersRouter");
+const adminRouter = require("./routes/adminRouter");
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +20,7 @@ app.use("/api/messages", messagesRouter);
 app.use("/api/drinks", drinksRouter);
 app.use("/api/boardgames", boardGamesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/admin", adminRouter);
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port + "!");
