f07fb92d037b6c8e71ca1d48bb7ce2c98125b55e
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-22 14:42:35 +0100

    Új funckiók beépítése

--- frontend/src/components/Helper_Components/Footer.jsx
+++ frontend/src/components/Helper_Components/Footer.jsx
@@ -1,40 +1,102 @@
+// function Footer() {
+//   return (
+//     <footer>
+//       <div className="links">
+//         <p>Készítette </p>
+//         <div className="nameCard">
+//           <a
+//             href="https://github.com/OsziBen"
+//             target="blank"
+//             rel="noopener noreferrer"
+//           >
+//             Bence
+//             <img id="githubIcon" src="/githubicon.png" alt="Github Icon" />
+//           </a>
+//         </div>
+//         <div className="nameCard">
+//           <a
+//             href="https://github.com/GergelyKN"
+//             target="blank"
+//             rel="noopener noreferrer"
+//           >
+//             Gergely
+//             <img id="githubIcon" src="/githubicon.png" alt="Github Icon" />
+//           </a>
+//         </div>
+//         <div className="nameCard">
+//           <a
+//             href="https://github.com/Abaku009"
+//             target="blank"
+//             rel="noopener noreferrer"
+//           >
+//             Szabolcs
+//             <img id="githubIcon" src="/githubicon.png" alt="Github Icon" />
+//           </a>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// }
+// export default Footer;
+
+import "./NavBarFooter.css";
+
 function Footer() {
   return (
-    <footer>
-      <div className="links">
-        <p>Készítette </p>
+    <footer className="footer">
+      <div className="footer-links">
+        <p>Készítette:</p>
         <div className="nameCard">
           <a
             href="https://github.com/OsziBen"
-            target="blank"
+            target="_blank"
             rel="noopener noreferrer"
+            className="footer-link"
           >
             Bence
-            <img id="githubIcon" src="/githubicon.png" alt="Github Icon" />
+            <img
+              id="githubIcon"
+              className="footer-icon"
+              src="/githubicon.png"
+              alt="Github Icon"
+            />
           </a>
         </div>
         <div className="nameCard">
           <a
             href="https://github.com/GergelyKN"
-            target="blank"
+            target="_blank"
             rel="noopener noreferrer"
+            className="footer-link"
           >
             Gergely
-            <img id="githubIcon" src="/githubicon.png" alt="Github Icon" />
+            <img
+              id="githubIcon"
+              className="footer-icon"
+              src="/githubicon.png"
+              alt="Github Icon"
+            />
           </a>
         </div>
         <div className="nameCard">
           <a
             href="https://github.com/Abaku009"
-            target="blank"
+            target="_blank"
             rel="noopener noreferrer"
+            className="footer-link"
           >
             Szabolcs
-            <img id="githubIcon" src="/githubicon.png" alt="Github Icon" />
+            <img
+              id="githubIcon"
+              className="footer-icon"
+              src="/githubicon.png"
+              alt="Github Icon"
+            />
           </a>
         </div>
       </div>
     </footer>
   );
 }
+
 export default Footer;
--- frontend/src/components/Helper_Components/NavBar.jsx
+++ frontend/src/components/Helper_Components/NavBar.jsx
@@ -1,28 +1,83 @@
-import { Link } from "react-router-dom";
+import "./NavBarFooter.css";
+
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function NavBar() {
-  return (
-    <nav>
-      <div className="logo">Logo</div>
-      <Link to="/" className="btn" id="MainArea">
-        Kezdőlap
-      </Link>
-      <Link to="/italok" className="btn" id="Drinks">
-        Italok
-      </Link>
-      <Link to="/tarsasjatekok" className="btn" id="BoardGames">
-        Társasok
-      </Link>
-      <Link to="/foglalas" className="btn" id="Reservation">
-        Foglalás
-      </Link>
-      <Link to="/kapcsolat" className="btn" id="Contact">
-        Kapcsolat
-      </Link>
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-      <Link to="/bejelentkezes" className="smallBtn" id="UserLogin">
-        Fiók Bejelentkezés
-      </Link>
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user")
+      ? JSON.parse(localStorage.getItem("user"))
+      : null;
+    setUser(storedUser);
+  }, []);
+
+  const handleLogOut = () => {
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("user");
+    setUser(null);
+    alert("Sikeres Kijelentkezés!");
+    navigate("/");
+  };
+
+  return (
+    <nav className="navbar">
+      <img className="navbar-logo" src="/logo_no_background.png" alt="Logo" />
+
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          Kezdőlap
+        </Link>
+        <Link to="/italok" className="nav-link">
+          Italok
+        </Link>
+        <Link to="/tarsasjatekok" className="nav-link">
+          Társasok
+        </Link>
+        <Link to="/foglalas" className="nav-link">
+          Foglalás
+        </Link>
+        <Link to="/kapcsolat" className="nav-link">
+          Kapcsolat
+        </Link>
+      </div>
+      <div className="navbar-user">
+        {user ? (
+          <>
+            {user.isadmin && (
+              <>
+                <Link to="/admin/italok" className="nav-admin-link">
+                  Admin Italok
+                </Link>
+                <Link to="/admin/tarsasjatekok" className="nav-admin-link">
+                  Admin Társasok
+                </Link>
+                <Link to="/admin/foglalas" className="nav-admin-link">
+                  Admin Foglalások
+                </Link>
+                <Link to="/admin/zartnapok" className="nav-admin-link">
+                  Admin Zárt Napok
+                </Link>
+              </>
+            )}
+            <Link to="/sajatfoglalasok" className="nav-small-link">
+              Saját foglalások
+            </Link>
+            <Link to="/profilszerkesztes" className="nav-small-link">
+              Profilom
+            </Link>
+            <button onClick={handleLogOut} className="nav-small-link">
+              Kijelentkezés
+            </button>
+          </>
+        ) : (
+          <Link to="/bejelentkezes" className="nav-small-link">
+            Fiók Bejelentkezés
+          </Link>
+        )}
+      </div>
     </nav>
   );
 }
