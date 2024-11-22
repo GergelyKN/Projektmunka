e956adb6c4fc7855235a3960508a339d538250f9
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-22 14:44:16 +0100

    Elfelejtett jelszó komponens létrehozása, kidolgozása

---
+++ frontend/src/components/RePassword/RePassword.jsx
@@ -0,0 +1,78 @@
+import NavBar from "../Helper_Components/NavBar";
+import Footer from "../Helper_Components/Footer";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+//Kötelező mező jelölése *-gal
+//Email küldés megoldása
+function RePassword() {
+  const [reEmail, setReEmail] = useState("");
+  const [redirectToLogin, setRedirectToLogin] = useState(false);
+
+  const handleEmailChange = (event) => {
+    setReEmail(event.target.value);
+  };
+  const RESETPASSWORDAPI = import.meta.env.VITE_API_RESETPASSWORD_URL;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const email = reEmail;
+      const response = await fetch(RESETPASSWORDAPI, {
+        mode: "cors",
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        const errorMessage = data.error;
+        if (response.status === 404) {
+          console.error("Hibaüzenet a szerver felől:", errorMessage);
+          alert(errorMessage);
+        }
+      } else {
+        alert(data.message);
+        setRedirectToLogin(true);
+      }
+    } catch (err) {
+      console.error("Hiba történt a kapcsolódáskor: ", err);
+    } finally {
+      cleanupAfterSubmit();
+    }
+  };
+
+  const cleanupAfterSubmit = () => {
+    setReEmail("");
+  };
+  if (redirectToLogin) {
+    return <Navigate to="/bejelentkezes" />;
+  }
+  return (
+    <>
+      <NavBar />
+
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h1>Jelszó emlékeztető</h1>
+          <label htmlFor="Email">Email cím: </label>
+          <input
+            type="email"
+            name="Email"
+            id="Email"
+            value={reEmail}
+            onChange={handleEmailChange}
+            required
+          />
+          <button type="submit">Elküldés</button>
+        </fieldset>
+      </form>
+      <Footer />
+    </>
+  );
+}
+export default RePassword;
