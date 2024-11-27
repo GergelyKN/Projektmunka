19df9a879c1650f7516782857c2ccb5983bc5861
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-28 00:19:20 +0100

    Új útvonalak a törléshez és lekérdezéshez

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -6,5 +6,7 @@ usersRouter.post("/registration", usersController.postNewUser);
 usersRouter.post("/login", usersController.postLogin);
 usersRouter.put("/updateuser", usersController.updateUser);
 usersRouter.put("/resetpassword", usersController.resetPassword);
+usersRouter.delete("/deleteuser", usersController.deleteUser);
+usersRouter.get("/getusers", usersController.getUsers);
 
 module.exports = usersRouter;
