7bf61c1efdede2547c83bcc751b48558c4d40018
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-29 16:30:17 +0100

    Ital leírás, mennyiség növelés

--- frontend/src/components/Admin/AdminDrinks/AdminDrink.jsx
+++ frontend/src/components/Admin/AdminDrinks/AdminDrink.jsx
@@ -1,14 +1,23 @@
-function AdminDrink({ drink, handleDelete, handleShowUpdateForm }) {
+function AdminDrink({
+  drink,
+  handleDelete,
+  handleShowUpdateForm,
+  handleAddQuantityShowForm,
+}) {
   return (
     <div className={"drink-" + drink.drinkid}>
       <h4>
         {drink.name + " - " + drink.price + " Ft - " + drink.size + " Liter "}
       </h4>
+      <h5>{drink.description}</h5>
       <button id="deleteButton" onClick={handleDelete}>
         Törlés
       </button>
       <button id="updateButton" onClick={handleShowUpdateForm}>
         Módosítás
+      </button>
+      <button id="addQuantityButton" onClick={handleAddQuantityShowForm}>
+        Mennyiség Növelése
       </button>
     </div>
   );
