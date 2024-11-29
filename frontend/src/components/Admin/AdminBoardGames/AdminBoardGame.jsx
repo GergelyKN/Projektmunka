6dfaa079c8d57ff30ef6474394353c551732a888
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-29 16:28:53 +0100

    Kiegészítes leírással és nyelvvel

--- frontend/src/components/Admin/AdminBoardGames/AdminBoardGame.jsx
+++ frontend/src/components/Admin/AdminBoardGames/AdminBoardGame.jsx
@@ -6,11 +6,14 @@ function AdminBoardGame({ boardgame, handleDelete, handleShowUpdateForm }) {
           " -- " +
           boardgame.difficulty +
           " -- " +
+          boardgame.language +
+          " -- " +
           boardgame.minplayernum +
           " - " +
           boardgame.maxplayernum +
           " Fő"}
       </h4>
+      <h5>{boardgame.description}</h5>
       <button id="deleteButton" onClick={handleDelete}>
         Törlés
       </button>
